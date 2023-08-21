ed3617c938b26c44ce0873ee85448c5dabf6b69f
SriSai_G <gandusrisai@gmail.com>
2023-08-21 18:57:07 +0530

    project structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotesComponent } from './components/notes/notes.component';
-import { StickyJsonComponent } from './services/sticky-json/sticky-json.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotesComponent,
-    StickyJsonComponent
   ],
   imports: [
     BrowserModule,
